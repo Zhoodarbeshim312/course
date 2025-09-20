01033f3e7aa0a2a50d027372d2fa80bb80d4e249
Joodarbeshim05 <162398776+Joodarbeshim05@users.noreply.github.com>
2025-09-20 17:57:34 +0600

    pofile...

--- src/components/authentication/registration/Registration.tsx
+++ src/components/authentication/registration/Registration.tsx
@@ -30,7 +30,10 @@ const Registration: FC = () => {
       setLoading(false);
     }
   };
-
+  const signGoogle = () => {
+    signInWithGoogle();
+    nav("/");
+  };
   return (
     <section className={scss.Registration}>
       <div className="container">
@@ -85,10 +88,7 @@ const Registration: FC = () => {
               <div className={scss.line}></div>
             </div>
             <div className={scss.btns}>
-              <button
-                onClick={() => signInWithGoogle()}
-                className={scss.google}
-              >
+              <button onClick={() => signGoogle()} className={scss.google}>
                 <FcGoogle />
                 Google
               </button>
